52e7e01496d8eda30c1db307cd356f0a6dfeb8e0
Dylan Perkins <dyllper@gmail.com>
2019-07-04 02:54:35 -0500

    Converting uploaded image into base64 byte string

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -10,12 +10,16 @@ const uploadImage = (req, res) => {
   form.parse(req, (err, fields, files) => {
     if (err) throw err;
     const fileToUpload = files.fileupload;
-    const oldPath = fileToUpload.path;
-    const newPath = `${config.uploadDir}${fileToUpload.name}`;
-    fs.rename(oldPath, newPath, (moveErr) => {
-      if (moveErr) throw moveErr;
-      res.json('File uploaded and moved!');
-    });
+    console.log(fileToUpload);
+    res.json('Testing');
+    const imageByte = Buffer.from(fs.readFileSync(fileToUpload.path)).toString('base64');
+    console.log(imageByte);
+    // const oldPath = fileToUpload.path;
+    // const newPath = `${config.uploadDir}${fileToUpload.name}`;
+    // fs.rename(oldPath, newPath, (moveErr) => {
+    //   if (moveErr) throw moveErr;
+    //   res.json('File uploaded and moved!');
+    // });
   });
 };
 
