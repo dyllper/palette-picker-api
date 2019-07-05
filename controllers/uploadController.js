7bc912cbba8807f6d208b89cb7860ab1a0ff3e56
Dylan Perkins <dyllper@gmail.com>
2019-07-04 22:35:33 -0500

    Breaking file upload into multiple functions using promises. Still need to get Clarifai call to work properly.

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,26 +1,46 @@
 const formidable = require('formidable');
+const Clarifai = require('clarifai');
 const fs = require('fs');
 
 const config = require('../config');
 
-const uploadImage = (req, res) => {
+const clarifaiApp = new Clarifai.App({
+  apiKey: 'keyGoesHere',
+});
+
+const parseFile = req => new Promise((resolve, reject) => {
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
-  form.type = 'multipart';
+  form.type = 'mutlipart';
   form.parse(req, (err, fields, files) => {
-    if (err) throw err;
-    const fileToUpload = files.fileupload;
-    console.log(fileToUpload);
-    res.json('Testing');
-    const imageByte = Buffer.from(fs.readFileSync(fileToUpload.path)).toString('base64');
-    console.log(imageByte);
-    // const oldPath = fileToUpload.path;
-    // const newPath = `${config.uploadDir}${fileToUpload.name}`;
-    // fs.rename(oldPath, newPath, (moveErr) => {
-    //   if (moveErr) throw moveErr;
-    //   res.json('File uploaded and moved!');
-    // });
+    if (err) reject(err);
+    resolve(files.fileupload);
   });
+});
+
+const convertFileToBytes = file => new Promise((resolve, reject) => {
+  try {
+    const byteFile = Buffer.from(fs.readFileSync(file.path)).toString('base64');
+    resolve(byteFile);
+  } catch (err) {
+    reject(err);
+  }
+});
+
+const makeClarifaiCall = (res, byteFile) => {
+  console.log('About to call clarifai');
+  clarifaiApp.models.predict(Clarifai.COLOR_MODEL, { base64: byteFile })
+    .then((response) => {
+      console.log(response);
+      res.json(response);
+    }).catch(err => res.status(400).json(err));
+};
+
+const uploadImage = (req, res) => {
+  parseFile(req)
+    .then(file => convertFileToBytes(file))
+    .then(fileBytes => makeClarifaiCall(res, fileBytes))
+    .catch(err => res.status(400).json(err));
 };
 
 module.exports = {
